0120a42c59907b900ac5091f650d92d22f1c702b
theoriginalsam <spoudel04@gmail.com>
2020-07-22 21:42:03 +0545

    Api update

--- src/Controller/ItemController.js
+++ src/Controller/ItemController.js
@@ -1,4 +1,18 @@
-exports.getFood = (req, res, next) => {};
+const Food = require("../models/Food");
+exports.getFood = (req, res) => {
+  Food.find()
+    .then((foods) => {
+      res
+        .status(200)
+        .json({ message: "Fetched foods successfully.", foods: foods });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      console.log("error");
+    });
+};
 exports.createFood = (req, res, next) => {};
 exports.updateFood = (req, res, next) => {};
 exports.deleteFood = (req, res, next) => {};
