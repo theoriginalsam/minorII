032c9cbffc5473d6d89d4c32513949cefaa83d4c
theoriginalsam <spoudel04@gmail.com>
2020-09-20 23:43:09 +0545

    Api update

--- src/routes/ItemRoute.js
+++ src/routes/ItemRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const ItemController = require("../Controller/ItemController");
-const Food = require("../models/Food");
+
 var multer = require("multer");
 const router = express.Router();
 
