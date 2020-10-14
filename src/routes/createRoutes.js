9778b64c113fc78d79d4e65262fd0afd7b77f8b3
theoriginalsam <spoudel04@gmail.com>
2020-10-14 19:55:51 +0545

    Api update

--- src/routes/createRoutes.js
+++ src/routes/createRoutes.js
@@ -14,7 +14,7 @@ router.post("/create", upload.single("photo"), async (req, res) => {
   const name = req.body.name;
   const category = req.body.category;
   //const photo = req.file.path;  //later for file
-  const photo = req.body.path;
+  const photo = req.body.photo;
   const price = req.body.price;
   //just create the api to store image path in the data base
   try {
